7968be807868eb811addb5a4850a9382df68df68
Nikos.p <connor2000@hotmail.gr>
2026-06-26 12:48:21 +0300

    refactor: rename default application class to Main

--- backend/src/main/java/com/example/leave_management_system/Main.java
+++ backend/src/main/java/com/example/leave_management_system/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LeaveManagementSystemApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LeaveManagementSystemApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
