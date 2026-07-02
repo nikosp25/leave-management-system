9955e4bffede04bc6715bd34f8d45f5c916ece16
Nikos.p <connor2000@hotmail.gr>
2026-07-02 18:39:46 +0300

    feat(security): configure open security filter chain

---
+++ backend/src/main/java/com/example/leave_management_system/security/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.leave_management_system.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // 1. Disable CSRF so you can freely test POST/PUT requests via Postman or React
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // 2. Tell Spring Security to allow every single request through without a password
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
