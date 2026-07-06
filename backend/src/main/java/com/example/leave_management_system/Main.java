b9a16c3d76e63065225ab5fb646a8f6cef334620
Nikos.p <connor2000@hotmail.gr>
2026-07-06 09:32:49 +0300

    feat: enable JPA auditing in main application

--- backend/src/main/java/com/example/leave_management_system/Main.java
+++ backend/src/main/java/com/example/leave_management_system/Main.java
@@ -2,8 +2,10 @@ package com.example.leave_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Main {
 
 	public static void main(String[] args) {
