e885b41df8db6d60bd4d3081967c49b445c6f289
Nikos.p <connor2000@hotmail.gr>
2026-07-09 14:16:11 +0300

    feat(security): configure stateless sessions and register JWT authentication filter

--- backend/src/main/java/com/example/leave_management_system/security/SecurityConfiguration.java
+++ backend/src/main/java/com/example/leave_management_system/security/SecurityConfiguration.java
@@ -1,35 +1,55 @@
 package com.example.leave_management_system.security;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // 1. Disable CSRF so you can freely test POST/PUT requests via Postman or React
+
                 .csrf(AbstractHttpConfigurer::disable)
 
-                // 2. Tell Spring Security to allow every single request through without a password
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
-                );
+                )
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); //add my custom filter before the default one
 
         return http.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
+
+
+
+
+
+
+
 
