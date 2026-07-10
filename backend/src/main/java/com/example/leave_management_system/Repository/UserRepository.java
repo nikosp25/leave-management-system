4a7cad0352abbfb94805c575bd2cd609bc9794eb
Nikos.p <connor2000@hotmail.gr>
2026-07-10 11:54:23 +0300

    feat(repository): add user fetch query with role and capabilities for authentication

--- backend/src/main/java/com/example/leave_management_system/Repository/UserRepository.java
+++ backend/src/main/java/com/example/leave_management_system/Repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.example.leave_management_system.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,6 +17,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 
     Optional<User> findByEmailAndDeletedFalse(String email);
+
+    @Query("""
+        SELECT DISTINCT u
+        FROM User u
+        JOIN FETCH u.role r
+        JOIN FETCH r.capabilities
+        WHERE u.email = :email
+        AND u.deleted = false
+    """)
+    Optional<User> findUserForAuthentication(@Param("email") String email); //Fetches the role and capabilities eagerly for the authentication process only
 
     Optional<User> findByEmail (String email);
 
