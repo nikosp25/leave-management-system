a0ff479c2de7ee0ba773858fd955f3d7bb7a8c17
Nikos.p <connor2000@hotmail.gr>
2026-07-26 07:36:11 +0300

    feat(page): add employee search page

---
+++ frontend/src/pages/EmployeeSearchPage.tsx
@@ -0,0 +1,7 @@
+import EmployeeSearch from '../components/dashboard/users/employees/EmployeeSearch'
+
+function EmployeeSearchPage() {
+    return <EmployeeSearch />
+}
+
+export default EmployeeSearchPage
